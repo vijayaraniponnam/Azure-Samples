eb6f2b9cbb2013cfde0ba413e65dc4479c842852
Vijayarani <vijayarani.ponnambalam@aspiresys.com>
2020-05-27 15:56:09 +0530

    azure function app sample

---
+++ FunctionApp1/Addition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using FunctionApp1.Model;
+using System.Net;
+using System.Text;
+
+namespace FunctionApp1
+{
+
+    /// <summary>
+    /// This function app used to perform the addtion operation by using two integers
+    /// </summary>
+    public static class Addition
+    {
+        [FunctionName("Addition")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "Addition")] HttpRequestMessage req)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+            bool success = true;
+            try
+            {
+                var request = req.RequestUri;
+                var modelValues = await req.Content.ReadAsStringAsync();
+                var inputElements = JsonConvert.DeserializeObject<Elements>(modelValues);
+
+                Elements elements = new Elements();
+                elements.number1 = inputElements.number1;
+                elements.number2 = inputElements.number2;
+                elements.result = inputElements.number1 + inputElements.number2;
+
+                var response = JsonConvert.SerializeObject(elements);
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(response, Encoding.UTF8, "application/json")
+                };
+            }
+            catch (Exception ex)
+            {
+                success = false;
+            }
+            return success
+                ? req.CreateResponse(HttpStatusCode.InternalServerError, "Unable to process your request!")
+                : req.CreateResponse(HttpStatusCode.BadRequest, "You have passed invalid integer");
+        }
+    }
+}
---
+++ FunctionApp1/Model/Elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp1.Model
+{
+   public class Elements
+    {
+       public int number1 { get; set; }
+       public int number2 { get; set; }
+       public int result { get; set; }
+    }
+}
+
